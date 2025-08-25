007c542fdc8b56bc15a6551f2ae6b514ac41e696
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-08-26 00:58:04 +0530

    understood all the singleton design patterns

---
+++ src/oop/SingletonMethods.java
@@ -0,0 +1,67 @@
+package oop;
+
+public class SingletonMethods {
+
+    //1 eager initialization
+//    public static SingletonMethods singleton = new SingletonMethods();
+//    private SingletonMethods(){
+//    }
+//    public static SingletonMethods getInstance(){
+//        return singleton;
+//    }
+
+    //2.Lazy initialization
+//    public static SingletonMethods lazyObj;
+//    private SingletonMethods(){
+//
+//    }
+//    public static SingletonMethods getInstance(){
+//        if(lazyObj==null){
+//            lazyObj = new SingletonMethods();
+//        }
+//        return lazyObj;
+//    }
+
+//3. Synchronised Method
+//    public static SingletonMethods synco;
+//    private SingletonMethods(){
+//
+//    }
+//    synchronized public static SingletonMethods getInstance(){
+//        if(synco==null){
+//            synco = new SingletonMethods();
+//        }
+//        return synco;
+//    }
+
+
+    //4.Double Check
+//    public static volatile SingletonMethods instance;
+//    private SingletonMethods(){
+//
+//    }
+//    public static SingletonMethods getInstance(){
+//        if(instance==null){
+//            synchronized (SingletonMethods.class){
+//                if(instance==null){
+//                    instance = new SingletonMethods();
+//                }
+//            }
+//        }
+//        return instance;
+//    }
+
+    //5. bill pugh
+//    private SingletonMethods(){
+//
+//    }
+//    private static class helper{
+//        private static final SingletonMethods billpough = new SingletonMethods();
+//    }
+//    public static SingletonMethods getInstance(){
+//        return helper.billpough;
+//    }
+
+
+
+}
