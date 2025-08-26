150aca995516512880eb338e453aee57c6752a9a
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-08-26 23:42:43 +0530

    feat: learned inheritance,super keyword and flow

---
+++ src/oop/inheritance/Box.java
@@ -0,0 +1,26 @@
+package oop.inheritance;
+
+public class Box {
+    int l;
+    int h;
+    int w;
+
+    Box(int l, int h, int w) {
+        this.l = l;
+        this.h = h;
+        this.w = w;
+
+    }
+    Box() {
+        this.l=0;
+        this.h=0;
+        this.w=0;
+    }
+
+    Box(Box old){
+        this.l=old.l;
+        this.h=old.h;
+        this.w=old.w;
+
+    }
+}
---
+++ src/oop/inheritance/Main.java
@@ -0,0 +1,11 @@
+package oop.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        BoxWeight obj =  new BoxWeight(11,12,14,10);
+        System.out.println(obj.l+" "+obj.h+" "+obj.w+" "+obj.weight);
+        BoxWeight obj2 = new BoxWeight(obj);
+        System.out.println(obj2.l+" "+obj2.h+" "+obj2.w+" "+obj2.weight);
+
+    }
+}
