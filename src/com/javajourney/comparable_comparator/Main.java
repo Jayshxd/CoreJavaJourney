a797226f57abea7bc138fe76ff30875b65d4c431
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-09-02 22:25:20 +0530

    feat : Learned about Comparable

---
+++ src/com/javajourney/comparable_comparator/Main.java
@@ -0,0 +1,24 @@
+package com.javajourney.comparable_comparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Jayesh",1,99));
+        students.add(new Student("Ramesh",3,92));
+        students.add(new Student("Suresh",2,23));
+        students.add(new Student("Mahesh",5,96));
+        students.add(new Student("Paresh",4,91));
+        System.out.println(students);
+        System.out.println();
+        Student s1 = students.getFirst();
+        System.out.println(s1);
+        Collections.sort(students);
+        System.out.println(students);
+    }
+
+
+
+}
