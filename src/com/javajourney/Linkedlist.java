60a572896e3b794de01005d838a975c9e004b4c8
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-09-07 21:44:27 +0530

    feat : Learned few methods of HashMap

---
+++ src/com/javajourney/Linkedlist.java
@@ -0,0 +1,13 @@
+package com.javajourney;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+public class Linkedlist {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        LinkedList<Integer> ll = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5,6,7,8,9,10));
+        ll.removeAll(al);
+        System.out.println(ll);
+
+    }
+}
